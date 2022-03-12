90c444c6a94af4cdf17bb4f059ca8f504a8b7690
poojarani1234 <101449774+poojarani1234@users.noreply.github.com>
2022-03-12 16:40:29 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,98 @@
+# def f1():
+#    s = "I Love Navgurukul"
+#    def f2():
+#     print(s)
+#    f2()
+
+# f1()
+
+# def num ( ):
+# 	i=1
+# 	while i<=10:
+# 		if i%2!=0:
+# 			print(i,'odd')
+# 		i=i+1
+# num()
+# def num ( ):
+# 	i=1
+# 	while i<=10:
+# 		if i%2==0:
+# 			print(i,'even')
+# 		i=i+1
+# num()
+# def num():
+# 	num1 = 56
+# 	num2 = 12
+# 	print(num1+num2)
+#num()
+# def num(a,b=20):
+# 	print(a+b)
+# num(2)
+# def isEven():
+#  if(12%2==0):
+#   print("Even Number")
+#  else:
+#   print("Old Number")
+# isEven()
+# 	def raw_a():
+# 	numbers = [3, 5, 7, 34, 2, 89, 2, 5]
+# 	i=0
+# 	min=numbers[i]
+# 	while i<len(numbers):
+# 		if numbers[i]<min:
+# 			min=numbers[i]
+# 		i+=1
+# 	print(min)
+# raw_a()
+# def reverse():
+# 	a= ["Z", "A", "A", "B", "E", "M", "A", "R", "D"]
+# 	i=0
+# 	while i<len(a):
+# 		a.reverse()
+# 		i+=1
+# 	print(a)
+# reverse()
+# def sort(a):
+# 	a = [6, 8, 4, 3, 9, 56, 0, 34, 7, 15]
+# 	i=0
+# 	while i<len(a):
+# 		j=0
+# 		while j<len(a):
+# 			if a[i]<a[j]:
+# 				a[i],a[j]=a[j],a[i]
+# 			j+=1
+# 		i+=1
+# 	print(a)
+# sort([6, 8, 4, 3, 9, 56, 0, 34,])
+# def num():
+# 	numbers = [1, 2, 3, 4, 5]
+# 	i=0
+# 	sum=0
+# 	while i<len(numbers):
+# 		sum=sum+numbers[i]
+# 		i+=1
+# 	print(sum)
+	
+# num()
+# def raw_a():
+# 	numbers = [3, 5, 7, 34, 2, 89, 2, 5]
+# 	i=0
+# 	sum=0
+# 	while i<len(numbers):
+# 		sum=sum+numbers[i]
+# 		i=i+1
+# 	print(sum)
+# raw_a()
+# def number( ):
+# 	i=0
+# 	while i<100:
+# 		print("I love you A")
+# 		i+=1
+# number()
+def sum():
+    age=int(input("enter a number"))
+    if  age>=18:
+        print("eligibal for voting")
+    else:
+        print("not eligibal for voting")
+sum()
